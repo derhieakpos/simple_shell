fb175f362209084113cbf18869c1037d78c02953
rachael-ojo <adeolaatilade@gmail.com>
2023-12-12 04:45:10 -0800

    main.h

---
+++ main.h
@@ -0,0 +1,16 @@
+#ifndef "main.h"
+#define "main.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+#define MAX_INPUT_SIZE 1024
+
+int customStrlen(const char *str);
+int customStrcmp(const char *str1, const char *str2)
+
+#endif
