e6af8f1d63295ffcd07bff42259125359b599351
rachael-ojo <adeolaatilade@gmail.com>
2023-12-13 06:25:15 -0800

    -m

--- ppid.c
+++ ppid.c
@@ -8,9 +8,9 @@
  */
 int main(void)
 {
-        pid_t my_ppid;
+	pid_t my_ppid;
 
-        my_ppid = getppid();
-        printf("%u\n",  my_ppid);
-        return (0);
+	my_ppid = getppid();
+	printf("%u\n",  my_ppid);
+	return (0);
 }
